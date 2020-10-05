19e36e780429d1ae630f178f50e08424a4ed8700
Byeonggon Lee <gonny952@gmail.com>
2020-10-05 22:07:17 +0900

    Add footer component

---
+++ nugurang-front/pages/signin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Footer from '../components/Footer';
+
+
+const useStyles = makeStyles((theme) => ({
+}));
+
+export default function singin() {
+  return (<Footer/>);
+}
+
