59fb3b303fd74b5a72cb1e612220fd7229080193
Byeonggon Lee <gonny952@gmail.com>
2020-10-28 02:20:06 +0900

    Fix error

--- nugurang-front/components/Loading.js
+++ nugurang-front/components/Loading.js
@@ -1,8 +1,6 @@
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Layout from './Layout';
 
-import Layout from './Layout';
-
 export default function Loading() {
   return <Layout><LinearProgress /></Layout>
 }
