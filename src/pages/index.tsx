452104258f5f247e99e7b3549f2372275f3cc3de
Kyeonghwan Son <snapperbay4453@gmail.com>
2022-01-18 04:14:05 +0900

    Apply theme props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,17 @@ import type { NextPage } from 'next'
 import styled from '@emotion/styled'
 
 const DivStyle = styled.div`
-  background-color: purple;
-  font-size: 24px;
-  border-radius: 4px;
-  padding: 32px;
-  text-align: center;
-  ${(props: any) => props.theme.mediaQuery.isTablet} {
+  ${(props: any) => `
+    background-color: ${props.theme.color.primary.main};
     color: white;
-  }
+    font-size: 24px;
+    border-radius: 4px;
+    padding: 32px;
+    text-align: center;
+    ${props.theme.mediaQuery.isMobile} {
+      background-color: ${props.theme.color.primary.dark};
+    }
+  `}
 `;
 
 const Home: NextPage = () => {
