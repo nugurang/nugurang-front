4379e4a69379cc6fdc4b51bce27e9093953028db
Kyeonghwan Son <snapperbay4453@gmail.con>
2022-07-29 18:26:09 +0900

    chore: Install Next.JS

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	parserPreset: "conventional-changelog-conventionalcommits",
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [
+			2,
+			"always",
+      [
+        "sentence-case"
+      ]
+    ]
+  }
+};
